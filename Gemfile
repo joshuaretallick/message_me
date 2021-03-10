3899d7d4c8d1f3cf8eec47f254607ffd1de96bc8
Joshua Retallick <josh@old-empire.co.uk>
2021-03-10 16:01:37 +0000

    Setup Heroku and Redis

--- Gemfile
+++ Gemfile
@@ -29,6 +29,7 @@ gem 'jquery-rails'
 # Use ActiveModel has_secure_password
 gem 'bcrypt', '~> 3.1.7'
 gem 'hirb'
+gem 'redis'
 
 # Use ActiveStorage variant
 # gem 'mini_magick', '~> 4.8'
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,5 +92,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   # config.action_cable.disable_request_forgery_protection = true
-  # config.action_cable.allowed_request_origins = ['https://herokuapp.com']
+  config.action_cable.allowed_request_origins = ['https://shrouded-spire-09874.herokuapp.com/']
 end
