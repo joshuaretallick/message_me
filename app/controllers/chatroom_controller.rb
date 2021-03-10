ae523558e63218b4e850e5735818a21c04dbe1ee
Joshua Retallick <josh@old-empire.co.uk>
2021-03-10 15:46:20 +0000

    Updated autoscroll and beautified message bar

--- app/controllers/chatroom_controller.rb
+++ app/controllers/chatroom_controller.rb
@@ -4,7 +4,7 @@ class ChatroomController < ApplicationController
 
     def index
         @message = Message.new
-        @messages = Message.all
+        @messages = Message.custom_display
     end
 
 end
