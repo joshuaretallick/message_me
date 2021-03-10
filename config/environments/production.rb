d4729306330605fa9eac33422913459af0ecffa7
Joshua Retallick <josh@old-empire.co.uk>
2021-03-10 15:27:14 +0000

    Set up ActionCable to async massge display in chatroom

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  # config.action_cable.disable_request_forgery_protection = true
+  # config.action_cable.allowed_request_origins = ['https://herokuapp.com']
 end
